951d619aaac5a9d4e6053c84fc3d5537e76e0f21
GeorgeVovos <gvovos@gmail.com>
2025-03-23 18:13:40 +0000

    tpt

--- MPI/Lecture04/Sum.c
+++ MPI/Lecture04/Sum.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 
+    int root = 0;
+
     int *array = (int *)malloc(N * sizeof(int));
     int *arrayAcc = (int *)malloc(N * sizeof(int));
     array[0] = 3;
@@ -22,6 +24,17 @@ int main(int argc, char **argv)
     array[5] = 5;
     // S={3,4,8,11,12,17}
 
+    // S={3,4,8}
+    // S={3,4,9}
+
+    // S={3,4}
+    // S={4,7}
+    // S={1,6}
+
+    double start, end;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    start = MPI_Wtime();
     arrayAcc[0] = array[0];
 
     for (int i = 1; i < N; i++)
@@ -29,12 +42,30 @@ int main(int argc, char **argv)
         arrayAcc[i] = arrayAcc[i - 1] + array[i];
     }
 
-    for (size_t i = 0; i < N; i++)
+    end = MPI_Wtime();
+    if (rank == 0)
     {
-        printf("%d ", arrayAcc[i]);
+        printf("Serial Time  : %f seconds\n", end - start);
     }
 
 
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (int i = 1; i < N; i++)
+    {
+        arrayAcc[i] = 0;
+    }
+
+    start = MPI_Wtime();
+
+    //Parallel
+    // for (size_t i = 0; i < N; i++)
+    // {
+    //     printf("%d ", arrayAcc[i]);
+    // }
+
+    // MPI_Bcast(array, N, MPI_INT, root, MPI_COMM_WORLD);
+    
     printf("\n");
     MPI_Finalize();
 
