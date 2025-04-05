5c98e56580db04d2b75700b2fb4c16d9cda8c993
GeorgeVovos <gvovos@gmail.com>
2025-04-05 20:02:33 +0100

    with load balancing but boundary conditions don't work

--- MPI/Lecture05/LoadBalance.c
+++ MPI/Lecture05/LoadBalance.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "mpi.h"
 #include "math.h"
-#define N 30
+#define N 31
 
 enum NotificationType 
 {
@@ -21,17 +21,17 @@ char **argv;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-    //printf("\n rank %d ",rank);
-    int proc_power = pow(2,rank);// Each node's processing power
+
+    int processorPower = pow(2,rank); //Not random, it's a power of 2 so I can easily check the results
     int *processorPowersArrayRaw = NULL;
     int *processorPowersArrayNormalized= NULL;
-    int *sourceArray = NULL;
-    int *resultArray = NULL;
+    float *sourceArray = NULL;
+    float *resultArray = NULL;
     if (rank == root) {
         processorPowersArrayRaw = (int *)malloc(worldSize * sizeof(int));
         processorPowersArrayNormalized = (int *)malloc(worldSize * sizeof(int));
-        sourceArray = (int *)malloc(N * sizeof(int));
-        resultArray = (int *)malloc(N * sizeof(int));
+        sourceArray = (float *)malloc(N * sizeof(float));
+        resultArray = (float *)malloc(N * sizeof(float));
         for (int i = 0; i < worldSize; i++)
         {
             processorPowersArrayRaw[i] = 0;
@@ -40,7 +40,7 @@ char **argv;
 
         for (int i = 0; i < N; i++)
         {
-            sourceArray[i] = 1;
+            sourceArray[i] = i;  // sourceArray[i] = 1;
             resultArray[i] = 0;
         }
     }
@@ -49,7 +49,7 @@ char **argv;
    // MPI_Bcast(&notificationType, 1, MPI_INT, root, MPI_COMM_WORLD);
 
     //if (notificationType == RequestProcessorPower) {
-        MPI_Gather(&proc_power, 1, MPI_INT, processorPowersArrayRaw, 1, MPI_INT, root, MPI_COMM_WORLD);
+        MPI_Gather(&processorPower, 1, MPI_INT, processorPowersArrayRaw, 1, MPI_INT, root, MPI_COMM_WORLD);
    // }
 
     if (rank == root) {
@@ -61,7 +61,6 @@ char **argv;
             totalPower += processorPowersArrayRaw[i];
         }
 
-       
         for (int i = 0; i < worldSize; i++) {
             processorPowersArrayNormalized[i] = (int)round((double)processorPowersArrayRaw[i] / totalPower * 100);
             totalNormalizedPower += processorPowersArrayNormalized[i];  
@@ -74,20 +73,59 @@ char **argv;
 
         printf("\nSource array ");
         for (int i = 0; i < N; i++) {
-            printf("%2d ", sourceArray[i]);
+            printf("%.2f ", sourceArray[i]);
         }
-        printf("\nResult array ");
+        // printf("\nResult array ");
 
-        for (int i = 0; i < N; i++) {
-            printf("%2d ", resultArray[i]);
-        }
+        // for (int i = 0; i < N; i++) {
+        //     printf("%.2f ", resultArray[i]);
+        // }
         printf("\n");
         free(processorPowersArrayRaw);
+    }
+
+    int *sendCounts = NULL;
+    int *displacements = NULL;
+    if (rank == root) {
+        sendCounts = (int *)malloc(worldSize * sizeof(int));
+        displacements = (int *)malloc(worldSize * sizeof(int));
+        int offset = 0;
+        for (int i = 0; i < worldSize; i++) {
+            sendCounts[i] = (int)round((double)processorPowersArrayNormalized[i] / 100 * N);
+            displacements[i] = offset;
+            offset += sendCounts[i];
+        }
+    }
+
+    int receiveCount = 0;
+    MPI_Scatter(sendCounts, 1, MPI_INT, &receiveCount, 1, MPI_INT, root, MPI_COMM_WORLD);
+
+    float *receivedArray = (float *)malloc(receiveCount * sizeof(float));
+    MPI_Scatterv(sourceArray, sendCounts, displacements, MPI_FLOAT, receivedArray, receiveCount, MPI_FLOAT, root, MPI_COMM_WORLD);
+
+    for (int i = 0; i < receiveCount; i++) {
+        receivedArray[i] = (float)(receivedArray[i] * 3.14);
+    }
+
+    printf("\nNode %d received %d elements ", rank, receiveCount);
+    printf("\n");
+    MPI_Gatherv(receivedArray, receiveCount, MPI_FLOAT, resultArray, sendCounts, displacements, MPI_FLOAT, root, MPI_COMM_WORLD);
+
+    free(receivedArray);
+    if (rank == root) {
+        free(sendCounts);
+        free(displacements);
+
+        printf("\nResult array ");
+        for (int i = 0; i < N; i++) {
+            printf("%.2f ", resultArray[i]);
+        }
+        printf("\n");
+        free(processorPowersArrayNormalized);
         free(sourceArray);
         free(resultArray);
     }
 
     MPI_Finalize();
-
     return 0;
 }
