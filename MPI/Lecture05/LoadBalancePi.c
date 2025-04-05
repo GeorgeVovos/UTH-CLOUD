31d8527600300c9fed8f12d542548317001e335b
GeorgeVovos <gvovos@gmail.com>
2025-04-05 20:32:14 +0100

    initial code for Pi , not working

--- MPI/Lecture05/LoadBalancePi.c
+++ MPI/Lecture05/LoadBalancePi.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "mpi.h"
 #include "math.h"
-#define N 31
+#define N 30
 
 enum NotificationType
 {
@@ -26,23 +26,22 @@ char **argv;
     int *processorPowersArrayRaw = NULL;
     int *processorPowersArrayNormalized = NULL;
     float *sourceArray = NULL;
-    float *resultArray = NULL;
+
     if (rank == root)
     {
         processorPowersArrayRaw = (int *)malloc(worldSize * sizeof(int));
         processorPowersArrayNormalized = (int *)malloc(worldSize * sizeof(int));
         sourceArray = (float *)malloc(N * sizeof(float));
-        resultArray = (float *)malloc(N * sizeof(float));
+
         for (int i = 0; i < worldSize; i++)
         {
             processorPowersArrayRaw[i] = 0;
             processorPowersArrayNormalized[i] = 0;
         }
 
-        for (int i = 0; i < N; i++)
+        for (int i = 1; i < N; i++)
         {
-            sourceArray[i] = i; // sourceArray[i] = 1;
-            resultArray[i] = 0;
+            sourceArray[i] = i;
         }
     }
 
@@ -73,11 +72,6 @@ char **argv;
         printf("Total processing power: %d\n", totalPower);
         printf("Total processing power normalized: %d\n", totalNormalizedPower);
 
-        printf("\nSource array ");
-        for (int i = 0; i < N; i++)
-        {
-            printf("%.2f ", sourceArray[i]);
-        }
         // printf("\nResult array ");
 
         // for (int i = 0; i < N; i++) {
@@ -87,7 +81,50 @@ char **argv;
         free(processorPowersArrayRaw);
     }
 
-   
+    int *sendCounts = NULL;
+    int *displacements = NULL;
+    int sendCountsSum = 0;
+    if (rank == root)
+    {
+        sendCounts = (int *)malloc(worldSize * sizeof(int));
+        displacements = (int *)malloc(worldSize * sizeof(int));
+        int offset = 0;
+        for (int i = 0; i < worldSize; i++)
+        {
+            sendCounts[i] = (N * processorPowersArrayNormalized[i]) / 100;
+            displacements[i] = offset;
+            offset += sendCounts[i];
+            sendCountsSum += sendCounts[i];
+        }
+
+        sendCounts[worldSize - 1] = sendCounts[worldSize - 1] + (N - sendCountsSum);
+    }
+
+    int localSize;
+    MPI_Scatter(sendCounts, 1, MPI_INT, &localSize, 1, MPI_INT, root, MPI_COMM_WORLD);
+    printf("Local size for rank %d: %d\n", rank, localSize);
+    float *localSource = (float *)malloc(localSize * sizeof(float));
+    MPI_Scatterv(sourceArray, sendCounts, displacements, MPI_FLOAT, localSource, localSize, MPI_FLOAT, root, MPI_COMM_WORLD);
+
+    double localPi = 1.0;
+    for (int i = 0; i < localSize; i++) {
+        localPi *= (4.0 * localSource[i] * localSource[i]) / (4.0 * localSource[i] * localSource[i] - 1);
+    }
+
+    double globalPi = 1.0;
+    MPI_Reduce(&localPi, &globalPi, 1, MPI_DOUBLE, MPI_PROD, root, MPI_COMM_WORLD);
+
+    if (rank == root)
+    {
+        globalPi *= 2.0;
+        printf("Calculated value of Pi: %.15f\n", globalPi);
+        free(sendCounts);
+        free(displacements);
+        free(sourceArray);
+        free(processorPowersArrayNormalized);
+    }
+
+    free(localSource);
     MPI_Finalize();
     return 0;
 }
