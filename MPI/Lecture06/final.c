62f625d21b7bbfe7dd202e1c7812a37de9bb68ce
GeorgeVovos <gvovos@gmail.com>
2025-04-08 15:24:34 +0100

    final, split work based on previous exercise

--- MPI/Lecture06/final.c
+++ MPI/Lecture06/final.c
@@ -1,19 +1,72 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main(int argc, char **argv)
+{
+    int root = 0;
 
-int main(int argc, char **argv) {
-     int size, rank;
-     int n=20;
+    int worldSize, rank;
+    int N = 20;
+    int a[] = {41, 467, 334, 500, 169, 724, 478, 358, 962, 464, 705, 145, 281, 827, 961, 491, 995, 942, 827, 436};
+    int processorPowersArrayRaw[] = {3, 2, 4, 1};
+    int processorPowersArrayNormalized[] = {0, 0, 0, 0};
 
-     MPI_Init(&argc, &argv);
-     MPI_Comm_size(MPI_COMM_WORLD, &size);
-     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-     if (rank == 0) {
-           int a[]={41, 467, 334, 500, 169, 724, 478, 358, 962, 464, 705, 145, 281, 827, 961, 491, 995, 942, 827, 436};
-           int proc_pow[] = {3, 2, 4, 1};
-     }
+    if (rank == root)
+    {
+        printf("Processing powers of all nodes:\n");
+        int totalPower = 0;
+        int totalNormalizedPower = 0;
+        for (int i = 0; i < worldSize; i++)
+        {
+            printf("Processor power raw - Node %2d: %2d\n", i, processorPowersArrayRaw[i]);
+            totalPower += processorPowersArrayRaw[i];
+        }
 
-     MPI_Finalize();
-     return 0;
+        for (int i = 0; i < worldSize; i++)
+        {
+            processorPowersArrayNormalized[i] = (int)round((double)processorPowersArrayRaw[i] / totalPower * 100);
+            totalNormalizedPower += processorPowersArrayNormalized[i];
+            printf("Processor power normalized - Node %2d: %2d\n", i, processorPowersArrayNormalized[i]);
+        }
+        printf("Total processing power: %d\n", totalPower);
+        printf("Total processing power normalized: %d\n", totalNormalizedPower);
+
+        printf("\n");
+    }
+
+    int *sendCounts = NULL;
+    int *displacements = NULL;
+    int sendCountsSum = 0;
+    if (rank == root)
+    {
+        sendCounts = (int *)malloc(worldSize * sizeof(int));
+        displacements = (int *)malloc(worldSize * sizeof(int));
+        int offset = 0;
+        for (int i = 0; i < worldSize; i++)
+        {
+            sendCounts[i] = (N * processorPowersArrayNormalized[i]) / 100;
+            displacements[i] = offset;
+            offset += sendCounts[i];
+            sendCountsSum += sendCounts[i];
+        }
+
+        sendCounts[worldSize - 1] = sendCounts[worldSize - 1] + (N - sendCountsSum);
+    }
+
+    if (rank == 0)
+    {
+        for (int i = 0; i < worldSize; i++)
+        {
+            printf("Local size for rank %d: %d\n", rank, sendCounts[i]);
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
